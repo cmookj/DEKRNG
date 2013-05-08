bfdcbfaa3dd4e536fe0cb16f1c3fec4bdaab164e
John C. Chun <cmookj@gmail.com>
2013-05-08 18:55:20 +0900

    Initial Commit

---
+++ RandomNumberGenerator/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RandomNumberGenerator
+//
+//  Created by Changmook Chun on 5/8/13.
+//  Copyright (c) 2013 Changmook Chun. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
